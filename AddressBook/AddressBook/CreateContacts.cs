8cd70b9f9a8ba6e2b2b3da252ebdf660c7632e18
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-23 00:51:57 +0530

    [Rajesh] Added : Create Contact Details

---
+++ AddressBook/AddressBook/CreateContacts.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class CreatContacts
+    {
+        //Class to fill the details
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string email;
+        public int zip;
+        public long phoneNumber;
+
+        public CreatContacts(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber)
+        {
+            //set values of class level variables from the constructor
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.email = email;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+        }
+
+        //Display the contact details
+        public void DisplayDetails()
+        {
+            Console.WriteLine("First Name : " + this.firstName);
+            Console.WriteLine("Last Name : " + this.lastName);
+            Console.WriteLine("Address : " + this.address);
+            Console.WriteLine("City : " + this.city);
+            Console.WriteLine("State : " + this.state);
+            Console.WriteLine("Email id : " + this.email);
+            Console.WriteLine("Zip code : " + this.zip);
+            Console.WriteLine("Phone number : " + this.phoneNumber);
+        }
+    }
+}
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System;
+namespace AddressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("============== Welcome to Address Book Program ===============");
+            int choice;
+            do
+            {
+                Console.WriteLine("\n1. Adding contacts details");
+
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Contact details are as shown below");
+                        //Adding contact information directly through object
+                        CreatContacts creatContacts = new CreatContacts("Rajesh", "Ambavale", "Dadar", "Mumbai", "Maharashtra", "rajesh@gmail.com", 4000016, 9967872990);
+                        creatContacts.DisplayDetails();
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter correct choice");
+                        break;
+                }
+            } while (choice != 0);
+        }
+    }
+}
